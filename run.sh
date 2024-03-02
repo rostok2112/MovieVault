a82cf7081b2aa70d7ab5ccc9a9fd627f3762b28d
little-endian <rostok21122112@gmail.com>
2024-03-02 21:48:38 +0200

    Minor fixes

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 
 set -ex # fail on any error and enable print of debug logs
 
-python manage.py  migrate # Migrate DB
-python manage.py collectstatic # Collect static
+python manage.py migrate # Migrate DB
+python manage.py collectstatic --no-input # Collect static
 python manage.py runserver 8005 --deploy # Run application
